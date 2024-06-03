f9127b154b2e08b3ac8fd984951708d2bdd36ad3
AndreaDeBonis <103332383+AndreaDeBonis@users.noreply.github.com>
2024-06-03 23:01:57 +0200

    trying to figure out how to implement projects

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import "./style.css";
 
 let tasks = [];
+let projects = [];
 const content = document.getElementById("content");
+const sidebar = document.getElementById("sidebar");
 let idCounter = 0;
 
-function createTask(title, description, dueDate, priority) {
+function createTask(title, description, project, dueDate, priority) {
   idCounter++;
   return {
     title,
     description,
+    project,
     dueDate,
     priority,
     toDo: false,
@@ -58,12 +61,24 @@ function renderTask(task) {
   <h2 class="text-2xl font-bold">${task.title}</h2>
   <ul class="text-xl mt-3 ml-4">
     <li>${task.description}</li>
+    <li>${task.project}</li>
     <li>${task.dueDate}</li>
     <li>${task.toDo}</li>
     <li>${task.priority}</li>
   </ul>
 </div>`;
   content.appendChild(renderedTask);
+}
+
+function renderProject(project) {
+  const newProject = document.createElement("div");
+  newProject.innerHTML = `<div>${project}</div>`;
+  sidebar.appendChild(newProject);
+}
+
+function renderMultiProjects() {
+  sidebar.innerHTML = "";
+  projects.forEach((el) => renderProject(el));
 }
 
 function renderMultiTasks(tasks) {
@@ -95,14 +110,23 @@ function renderMultiTasks(tasks) {
   });
 })();
 
-const submitBtn = document.getElementById("submit");
-
 function submitTask() {
   const title = document.getElementById("title").value;
   const description = document.getElementById("description").value;
+  const project = document.getElementById("project").value;
   const dueDate = document.getElementById("dueDate").value;
   const priority = document.getElementById("priority").value;
-  const task = createTask(title, description, dueDate, priority);
+  const task = createTask(title, description, project, dueDate, priority);
   addTask(task);
+  addProject(task.project);
   renderMultiTasks(tasks);
+  renderMultiProjects();
+  console.log(projects);
+}
+
+function addProject(proj) {
+  if (projects.includes(proj)) {
+    return 0;
+  }
+  projects.push(proj);
 }
