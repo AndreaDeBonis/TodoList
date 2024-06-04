292083a3a572ebaf3ad12a5eae728c43fa9eb8f5
AndreaDeBonis <103332383+AndreaDeBonis@users.noreply.github.com>
2024-06-04 18:33:52 +0200

    Done some UI rework and added filtering

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import "./style.css";
+import {
+  parse,
+  format,
+  startOfISOWeek,
+  endOfISOWeek,
+  isWithinInterval,
+} from "date-fns";
 
 let tasks = [];
-let projects = [];
+let projects = ["Default"];
 const content = document.getElementById("content");
-const sidebar = document.getElementById("sidebar");
+const projectsContainer = document.getElementById("projects-container");
+let currentProject = "Default";
+let timeFilter = "Week";
+document.getElementById("default").checked = true;
 let idCounter = 0;
 
 function createTask(title, description, project, dueDate, priority) {
@@ -25,7 +35,38 @@ function printTask(task) {
   console.log(task.dueDate);
 }
 
-function filterTasks(tasks, filter) {
+function getTasks() {
+  const tasksByProject = tasks.filter(
+    (task) => task.project === currentProject
+  );
+  if (timeFilter === "All") {
+    return tasksByProject;
+  }
+  if (timeFilter === "Today") {
+    return filterToday(tasksByProject);
+  }
+  if (timeFilter === "Week") {
+    return filterWeek(tasksByProject);
+  }
+}
+
+function filterToday(list) {
+  return list.filter(
+    (task) =>
+      format(task.dueDate, "MM-dd-yyyy") === format(new Date(), "MM-dd-yyyy")
+  );
+}
+
+function filterWeek(list) {
+  return list.filter((task) =>
+    isWithinInterval(task.dueDate, {
+      start: startOfISOWeek(new Date()),
+      end: endOfISOWeek(new Date()),
+    })
+  );
+}
+
+/* function filterTasks(tasks, filter) {
   let filteredTasks = [...tasks];
   for (let key in filter) {
     filteredTasks = filteredTasks.filter((task) => task[key] == filter[key]);
@@ -33,7 +74,7 @@ function filterTasks(tasks, filter) {
 
   return filteredTasks;
 }
-
+ */
 function printAllTasks(tasks, filter) {
   tasks.forEach((task) => printTask(task));
 }
@@ -54,7 +95,9 @@ const filter = { dueDate: "oggi", description: "ovvio" };
 
 function renderTask(task) {
   const renderedTask = document.createElement("div");
-  renderedTask.innerHTML = `<div data-id="${task.id}" class="relative px-24 py-8 border">
+  renderedTask.innerHTML = `<div data-id="${
+    task.id
+  }" class="relative px-12 py-8 border">
   <button class="bg-red-300 absolute right-4 px-2 py-1" id="delete">
             X
           </button>
@@ -62,7 +105,7 @@ function renderTask(task) {
   <ul class="text-xl mt-3 ml-4">
     <li>${task.description}</li>
     <li>${task.project}</li>
-    <li>${task.dueDate}</li>
+    <li>${format(task.dueDate, "MM-dd-yyy")}</li>
     <li>${task.toDo}</li>
     <li>${task.priority}</li>
   </ul>
@@ -72,12 +115,29 @@ function renderTask(task) {
 
 function renderProject(project) {
   const newProject = document.createElement("div");
-  newProject.innerHTML = `<div>${project}</div>`;
-  sidebar.appendChild(newProject);
+  newProject.innerHTML = `
+  <input
+    type="radio"
+    id="${project}"
+    name="projects"
+    value="${project}"
+    class="peer hidden"
+  />
+  <label
+    for="${project}"
+    data-project="${project}"
+    id="projectswitch"
+    class="grow w-12 px-3 py-2 rounded border border-slate-300 peer-checked:bg-slate-300"
+  >
+  ${project} </label
+  ><br />`;
+  newProject.classList.add("w-full", "flex");
+  projectsContainer.appendChild(newProject);
+  document.getElementById(project).checked = true;
 }
 
 function renderMultiProjects() {
-  sidebar.innerHTML = "";
+  projectsContainer.innerHTML = "";
   projects.forEach((el) => renderProject(el));
 }
 
@@ -107,20 +167,37 @@ function renderMultiTasks(tasks) {
       removeTask(taskContainer.dataset.id);
       taskContainer.parentElement.remove();
     }
+    if (e.target.id === "submit-project") {
+      const project = document.getElementById("project").value;
+      addProject(project);
+      renderMultiProjects();
+    }
+    if (e.target.id === "projectswitch") {
+      currentProject = e.target.dataset.project;
+      console.log(getTasks());
+      renderMultiTasks(getTasks());
+    }
+    if (e.target.id === "filter") {
+      timeFilter = e.target.dataset.filter;
+      renderMultiTasks(getTasks());
+    }
   });
 })();
 
 function submitTask() {
   const title = document.getElementById("title").value;
   const description = document.getElementById("description").value;
-  const project = document.getElementById("project").value;
-  const dueDate = document.getElementById("dueDate").value;
+  const project = currentProject;
+  console.log(document.getElementById("dueDate").value);
+  const dueDate = parse(
+    document.getElementById("dueDate").value,
+    "yyyy-MM-dd",
+    new Date()
+  );
   const priority = document.getElementById("priority").value;
   const task = createTask(title, description, project, dueDate, priority);
   addTask(task);
-  addProject(task.project);
-  renderMultiTasks(tasks);
-  renderMultiProjects();
+  renderMultiTasks(getTasks());
   console.log(projects);
 }
 
@@ -130,3 +207,6 @@ function addProject(proj) {
   }
   projects.push(proj);
 }
+
+console.log(format(new Date(), "MM-dd-yyyy"));
+console.log(startOfISOWeek(new Date()));
